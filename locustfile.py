442fa99dce73c4e45770e2428cb622db5d0657b3
Brikkel@Crush <rikvanbrakel24@gmail.com>
2023-04-14 14:40:17 +0200

    fixed bug

--- locustfile.py
+++ locustfile.py
@@ -9,25 +9,25 @@ class LoadTester(HttpUser):
         amount_items = randint(0,5)
 
         for i in range(amount_items):
-            item = randint(0,80)
+            item = randint(1,80)
             self.client.get(f"/products/{item}")
     
     @task(2)
     def existing_user(self):
-        user = randint(0,20)
-        self.client.get(f"/users/{user}")
+        user = randint(1,20)
+        self.client.get(f"/customers/{user}")
         self.client.get("/products")
         
         amount_cat = randint(0,3)
         for i in range(amount_cat):
-            cat = randint(0,6)
+            cat = randint(1,6)
             self.client.get(f"/categories/{cat}")
 
 
         amount_items = randint(0,15)
 
         for i in range(amount_items):
-            item = randint(0,80)
+            item = randint(1,80)
             self.client.get(f"/products/{item}")
 
 
